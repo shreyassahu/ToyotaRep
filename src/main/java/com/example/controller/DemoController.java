3e30eddef97df6d5b62d97f493771f4decf67055
shreyas.sahu <shreyassahu2208@gmail.com>
2022-08-01 14:24:59 +0530

    Added method in DemoController

--- src/main/java/com/example/controller/DemoController.java
+++ src/main/java/com/example/controller/DemoController.java
@@ -1,8 +1,15 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+	
+	@RequestMapping(value = "/")
+	public String method1()
+	{
+		return ("Hi");
+	}
 	
 }
